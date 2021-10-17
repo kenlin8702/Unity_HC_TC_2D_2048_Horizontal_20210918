04e8a15987e7e99584688e56b56c04019e98556a
kenlin8702 <kenlin8702@yahoo.com.tw>
2021-10-17 13:00:36 +0800

    程式練習

---
+++ 2048_Horizontal_20210918/Assets/Script/Car.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+
+//classÃş§OÂÅ¹Ï
+public class Car : MonoBehaviour
+{
+    public int cc = 1500;
+    public float weight = 3000f;
+    public string brand = "BMW";
+    public bool skywindow = true;
+    
+}
