097150a9057e6dc4f1272f5fcb143970abe69f43
kenlin8702 <kenlin8702@yahoo.com.tw>
2021-11-14 13:12:26 +0800

    開始畫面管理器+音量

---
+++ 2048_Horizontal_20210918/Assets/Script/MenuManager.cs
@@ -0,0 +1,36 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;//引用場景管理
+using UnityEngine.Audio;
+/// <summary>
+/// Event管理器
+/// </summary>
+public class MenuManager : MonoBehaviour
+{
+    
+    public AudioMixer mixer;
+    /// <summary>
+    /// 開始遊戲
+    /// </summary>
+    public void StartGame() {
+        //載入場景
+        SceneManager.LoadScene("遊戲畫面");
+    }
+    /// <summary>
+    /// 設定遊戲
+    /// </summary>
+    public void SettingGameBGM(float sound) {
+        mixer.SetFloat("音量BGM", sound);
+    }
+    public void SetteingGameSFX(float sound) {
+        mixer.SetFloat("音量SFX", sound);
+    }
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+    public void QuitButton() {
+        Application.Quit();
+
+        print("離開遊戲");
+    }
+}
